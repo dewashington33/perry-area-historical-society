0237b2bf5332c51261aa9ef702d9eba22be94c11
dewashington33 <dewashington@hotmail.com>
2024-02-14 00:31:21 -0500

    Refactor routes and import statements in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,27 @@
 import { Route, Routes } from "react-router-dom";
+import FooterPAHS from "./components/FooterPAHS";
+import HeaderPAHS from "./components/HeaderPAHS";
+import Articles from "./pages/Articles";
 import Home from "./pages/Home";
 import Topics from "./pages/Topics";
-import Articles from "./pages/Articles";
-import HeaderPAHS from "./components/HeaderPAHS";
-import FooterPAHS from "./components/FooterPAHS";
 
+const homeRoute =
+  import.meta.env.MODE === "production"
+    ? "/perry-area-historical-society"
+    : "/";
 
 function App() {
   return (
     <div>
-    <HeaderPAHS />
-    
+      <HeaderPAHS />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path={homeRoute} element={<Home />} />
         <Route path="/topics" element={<Topics />} />
         <Route path="/articles" element={<Articles />} />
       </Routes>
       <FooterPAHS />
-      </div>
+    </div>
   );
-
 }
 
 export default App;
